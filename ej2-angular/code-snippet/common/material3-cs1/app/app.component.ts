0620856c036b0f10aed352fc260dc816131cc3b7
Build Automaion <buildautomation@syncfusion.com>
2023-08-16 18:02:14 +0530

    Integrated latest changes at 08-16-2023 2:32:13 PM

--- ej2-angular/code-snippet/common/material3-cs1/app/app.component.ts
+++ ej2-angular/code-snippet/common/material3-cs1/app/app.component.ts
@@ -3,7 +3,24 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     styleUrls:['./style.css'],
-    templateUrl:'app.component.html'  
+    template:`<div [ngClass]="{'e-dark-mode': isChecked, 'dark': isChecked}">
+    <ejs-checkbox label="Enable Darkmode" (change)="toggleCheckbox()"></ejs-checkbox><br/>
+   
+     <!-- Primary Button - Used to represent a primary action. -->
+     <button ejs-button cssClass="e-primary">Button</button>
+   
+     <!-- Success Button - Used to represent a positive action. -->
+     <button ejs-button cssClass="e-success">Button</button>
+   
+     <!-- Info Button - Used to represent an informative action -->
+     <button ejs-button cssClass="e-info">Button</button>
+   
+     <!-- Warning Button - Used to represent an action with caution. -->
+     <button ejs-button cssClass="e-warning">Button</button>
+   
+     <!-- Danger Button - Used to represent a negative action. -->
+     <button ejs-button cssClass="e-danger">Button</button>
+     </div>`  
 })
 
 export class AppComponent { 
