b1647313edbf7abb95385eb664faa4ba87e5e953
Build Automaion <buildautomation@syncfusion.com>
2023-08-18 14:34:58 +0530

    Integrated latest changes at 08-18-2023 12:35:47 PM

---
+++ ej2-angular/Release-notes/migration-16.1-to-16.2.md
@@ -0,0 +1,16 @@
+---
+title: Essential Studio for Angular Weekly Nuget Release Release Notes  
+description: Essential Studio for Angular Weekly Nuget Release Release Notes  
+platform: ej2-angular
+
+documentation: ug
+---
+
+# Essential Studio for  Angular  Release Notes  
+
+
+{% directory path: _includes/release-notes/migration-16.1-to-16.2 %}
+
+{% include {{file.url}} %}
+
+{% enddirectory %}
--- ej2-angular/code-snippet/grid/searching-cs9/app/app.component.ts
+++ ej2-angular/code-snippet/grid/searching-cs9/app/app.component.ts
@@ -9,11 +9,11 @@ import { ToolbarItems,SearchSettingsModel,GridComponent } from '@syncfusion/ej2-
     template: `
     <div>
     <label style="padding: 10px 10px">
-    Change ignoreAccent property
+    Enable or disable ignoreAccent property
     </label>
     <ejs-switch id="switch" (change)="onSwitchChange($event)"></ejs-switch>
     </div>
-    <ejs-grid #grid [dataSource]='data' [toolbar]='toolbarOptions' [searchSettings]='searchOptions' height='272px'>
+    <ejs-grid #grid [dataSource]='data' [toolbar]='toolbarOptions' height='272px'>
         <e-columns>
             <e-column field='CategoryName' headerText='Category Name' width='100'></e-column>
             <e-column field='ProductName' headerText='Product Name' width='130'></e-column>
@@ -26,23 +26,19 @@ export class AppComponent implements OnInit {
 
     public data?: object[];
     public toolbarOptions?: ToolbarItems[];
-    public searchOptions?: SearchSettingsModel;
     @ViewChild('grid')
     public grid?: GridComponent;
 
     ngOnInit(): void {
         this.data = data;
         this.toolbarOptions = ['Search'];
-        this.searchOptions = {
-            ignoreAccent: true
-        };
     }
 
     onSwitchChange(args: any) {
         if (args.checked) {
-            this.grid.searchSettings.ignoreAccent = false;
+            (this.grid as any).searchSettings.ignoreAccent = true;
         } else {
-            this.grid.searchSettings.ignoreAccent = true;
+            (this.grid as any).searchSettings.ignoreAccent = false;
         }
     }
 }
