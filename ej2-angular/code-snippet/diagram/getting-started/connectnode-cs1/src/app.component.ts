de081899cd1a584049e053e37401679f5bfef981
Build Automaion <buildautomation@syncfusion.com>
2023-11-04 02:47:20 +0530

    Integrated latest changes at 11-04-2023 1:31:02 AM

--- ej2-angular/code-snippet/diagram/getting-started/connectnode-cs1/src/app.component.ts
+++ ej2-angular/code-snippet/diagram/getting-started/connectnode-cs1/src/app.component.ts
@@ -1,7 +1,7 @@
 
 
 import { Component, ViewEncapsulation, ViewChild } from "@angular/core";
-import { BasicShapeModel } from "@syncfusion/ej2-angular-diagrams";
+import { FlowShapeModel  } from "@syncfusion/ej2-angular-diagrams";
 
 @Component({
   selector: "app-container",
@@ -12,23 +12,15 @@ import { BasicShapeModel } from "@syncfusion/ej2-angular-diagrams";
                 <e-node-annotation content='Start'></e-node-annotation>
             </e-node-annotations>
         </e-node>
-        <e-node id='node2' [height]=60 [width]=100 [offsetX]=300 [offsetY]=160 [shape]='shape'>
-            <e-node-annotations>
-                <e-node-annotation content='var i = 0;'></e-node-annotation>
-            </e-node-annotations>
-        </e-node>
     </e-nodes>
-    <e-connectors>
-        <e-connector id='connector1' sourceID='node1' targetID='node2'></e-connector>
-    </e-connectors>
   </ejs-diagram>`,
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   @ViewChild("diagram")
-  public shape?: BasicShapeModel;
+  public shape?: FlowShapeModel ;
   ngOnInit(): void {
-    this.shape = { type: "Basic", shape: "Rectangle" };
+    this.shape = { type: "Flow", shape: "Terminator" };
   }
 }
 
