e14a506158e27ea25254a74a312617faeafbda2c
Build Automaion <buildautomation@syncfusion.com>
2023-12-01 02:52:05 +0530

    Integrated latest changes at 11-30-2023 10:30:20 PM

---
+++ ej2-angular/code-snippet/spreadsheet/insert-sheet-change-active-sheet-cs1/src/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, ViewChild } from '@angular/core';
+import { SpreadsheetComponent } from '@syncfusion/ej2-angular-spreadsheet';
+import { data, employeeData } from './datasource';
+
+@Component({
+    selector: 'app-container',
+    template: `<div >
+    <button class="e-btn" (click)='onClick()'>Insert Sheet</button>
+    <ejs-spreadsheet #spreadsheet >
+        <e-sheets>
+            <e-sheet name="Car Sales Report">
+                <e-ranges>
+                    <e-range [dataSource]="dataSource"></e-range>
+                </e-ranges>
+                <e-columns>
+                    <e-column [width]=180></e-column>
+                    <e-column [width]=130></e-column>
+                    <e-column [width]=130></e-column>
+                    <e-column [width]=180></e-column>
+                    <e-column [width]=130></e-column>
+                    <e-column [width]=120></e-column>
+                </e-columns>
+            </e-sheet>
+        </e-sheets>
+    </ejs-spreadsheet>
+</div>`
+})
+export class AppComponent {
+    @ViewChild('spreadsheet')
+    spreadsheetObj!: SpreadsheetComponent;
+    dataSource: Object[] = data;
+    onClick(): void {
+        this.spreadsheetObj.insertSheet(
+            [
+                {
+                    index: 1,
+                    name: 'new_sheet',
+                    ranges: [
+                        {
+                            dataSource: employeeData,
+                            startCell: 'A1'
+                        },
+                    ]
+                },
+            ]
+        );
+        // Use the timeout function to wait until the sheet is inserted.
+        setTimeout(() => {
+            // Method for switching to a new sheet.
+            this.spreadsheetObj.goTo('new_sheet!A1');
+        })
+    }
+}
---
+++ ej2-angular/code-snippet/spreadsheet/insert-sheet-change-active-sheet-cs1/src/datasource.ts
@@ -0,0 +1,688 @@
+
+export let data: Object[] = [
+    {
+        "Customer Name": "Romona Heaslip",
+        "Model": "Taurus",
+        "Color": "Aquamarine",
+        "Payment Mode": "Debit Card",
+        "Delivery Date": "07/11/2015",
+        "Amount": "8529.22"
+    },
+    {
+        "Customer Name": "Clare Batterton",
+        "Model": "Sparrow",
+        "Color": "Pink",
+        "Payment Mode": "Cash On Delivery",
+        "Delivery Date": "7/13/2016",
+        "Amount": "17866.19"
+    },
+    {
+        "Customer Name": "Eamon Traise",
+        "Model": "Grand Cherokee",
+        "Color": "Blue",
+        "Payment Mode": "Net Banking",
+        "Delivery Date": "09/04/2015",
+        "Amount": "13853.09"
+    },
+    {
+        "Customer Name": "Julius Gorner",
+        "Model": "GTO",
+        "Color": "Aquamarine",
+        "Payment Mode": "Credit Card",
+        "Delivery Date": "12/15/2017",
+        "Amount": "2338.74"
+    },
+    {
+        "Customer Name": "Jenna Schoolfield",
+        "Model": "LX",
+        "Color": "Yellow",
+        "Payment Mode": "Credit Card",
+        "Delivery Date": "10/08/2014",
+        "Amount": "9578.45"
+    },
+    {
+        "Customer Name": "Marylynne Harring",
+        "Model": "Catera",
+        "Color": "Green",
+        "Payment Mode": "Cash On Delivery",
+        "Delivery Date": "7/01/2017",
+        "Amount": "19141.62"
+    },
+    {
+        "Customer Name": "Vilhelmina Leipelt",
+        "Model": "7 Series",
+        "Color": "Goldenrod",
+        "Payment Mode": "Credit Card",
+        "Delivery Date": "12/20/2015",
+        "Amount": "6543.30"
+    },
+    {
+        "Customer Name": "Barby Heisler",
+        "Model": "Corvette",
+        "Color": "Red",
+        "Payment Mode": "Credit Card",
+        "Delivery Date": "11/24/2014",
+        "Amount": "13035.06"
+    },
+    {
+        "Customer Name": "Karyn Boik",
+        "Model": "Regal",
+        "Color": "Indigo",
+        "Payment Mode": "Debit Card",
+        "Delivery Date": "05/12/2014",
+        "Amount": "18488.80"
+    },
+    {
+        "Customer Name": "Jeanette Pamplin",
+        "Model": "S4",
+        "Color": "Fuscia",
+        "Payment Mode": "Net Banking",
+        "Delivery Date": "12/30/2014",
+        "Amount": "12317.04"
+    },
+    {
+        "Customer Name": "Cristi Espinos",
+        "Model": "TL",
+        "Color": "Aquamarine",
+        "Payment Mode": "Credit Card",
+        "Delivery Date": "12/18/2013",
+        "Amount": "6230.13"
+    },
+    {
+        "Customer Name": "Issy Humm",
+        "Model": "Club Wagon",
+        "Color": "Pink",
+        "Payment Mode": "Cash On Delivery",
+        "Delivery Date": "02/02/2015",
+        "Amount": "9709.49"
+    },
+    {
+        "Customer Name": "Tuesday Fautly",
+        "Model": "V8 Vantage",
+        "Color": "Crimson",
+        "Payment Mode": "Debit Card",
+        "Delivery Date": "11/19/2014",
+        "Amount": "9766.10"
+    },
+    {
+        "Customer Name": "Rosemaria Thomann",
+        "Model": "Caravan",
+        "Color": "Violet",
+        "Payment Mode": "Net Banking",
+        "Delivery Date": "02/08/2014",
+        "Amount": "7685.49"
+    },
+    {
+        "Customer Name": "Lyell Fuentez",
+        "Model": "Bravada",
+        "Color": "Violet",
+        "Payment Mode": "Debit Card",
+        "Delivery Date": "08/05/2016",
+        "Amount": "18012.45"
+    },
+    {
+        "Customer Name": "Raynell Layne",
+        "Model": "Colorado",
+        "Color": "Pink",
+        "Payment Mode": "Credit Card",
+        "Delivery Date": "05/30/2016",
+        "Amount": "2785.49"
+    },
+    {
+        "Customer Name": "Raye Whines",
+        "Model": "4Runner",
+        "Color": "Red",
+        "Payment Mode": "Debit Card",
+        "Delivery Date": "12/10/2016",
+        "Amount": "9967.74"
+    },
+    {
+        "Customer Name": "Virgina Aharoni",
+        "Model": "TSX",
+        "Color": "Pink",
+        "Payment Mode": "Cash On Delivery",
+        "Delivery Date": "10/23/2014",
+        "Amount": "5584.33"
+    },
+    {
+        "Customer Name": "Peta Cheshir",
+        "Model": "Pathfinder",
+        "Color": "Red",
+        "Payment Mode": "Net Banking",
+        "Delivery Date": "12/24/2015",
+        "Amount": "5286.53"
+    },
+    {
+        "Customer Name": "Jule Urion",
+        "Model": "Charger",
+        "Color": "Violet",
+        "Payment Mode": "Debit Card",
+        "Delivery Date": "11/20/2013",
+        "Amount": "13511.91"
+    },
+    {
+        "Customer Name": "Lew Gilyatt",
+        "Model": "Bonneville",
+        "Color": "Crimson",
+        "Payment Mode": "Credit Card",
+        "Delivery Date": "11/19/2013",
+        "Amount": "6498.19"
+    },
+    {
+        "Customer Name": "Jobey Fortun",
+        "Model": "B-Series",
+        "Color": "Blue",
+        "Payment Mode": "Net Banking",
+        "Delivery Date": "10/30/2014",
+        "Amount": "10359.67"
+    },
+    {
+        "Customer Name": "Blondie Crump",
+        "Model": "Voyager",
+        "Color": "Turquoise",
+        "Payment Mode": "Credit Card",
+        "Delivery Date": "04/06/2018",
+        "Amount": "8118.39"
+    },
+    {
+        "Customer Name": "Florentia Binns",
+        "Model": "Grand Prix",
+        "Color": "Orange",
+        "Payment Mode": "Cash On Delivery",
+        "Delivery Date": "10/13/2016",
+        "Amount": "10204.37"
+    },
+    {
+        "Customer Name": "Jaquelin Galtone",
+        "Model": "Sunbird",
+        "Color": "Red",
+        "Payment Mode": "Net Banking",
+        "Delivery Date": "10/22/2013",
+        "Amount": "6528.06"
+    },
+    {
+        "Customer Name": "Hakeem Easseby",
+        "Model": "Mirage",
+        "Color": "Crimson",
+        "Payment Mode": "Debit Card",
+        "Delivery Date": "9/12/2014",
+        "Amount": "5619.25"
+    },
+    {
+        "Customer Name": "Nickolaus Gidman",
+        "Model": "XK",
+        "Color": "Orange",
+        "Payment Mode": "Debit Card",
+        "Delivery Date": "05/12/2016",
+        "Amount": "5091.43"
+    },
+    {
+        "Customer Name": "Jenine Iglesia",
+        "Model": "Accord",
+        "Color": "Orange",
+        "Payment Mode": "Debit Card",
+        "Delivery Date": "09/03/2018",
+        "Amount": "14566.08"
+    },
+    {
+        "Customer Name": "Fax Witherspoon",
+        "Model": "Range Rover Sport",
+        "Color": "Orange",
+        "Payment Mode": "Credit Card",
+        "Delivery Date": "2/22/2018",
+        "Amount": "5284.87"
+    }
+  ];
+  
+  export let employeeData: Object[] = [
+    {
+        "Employee ID": "5389863",
+        "Employee Name": "Liuka Tewkesberry",
+        "Gender": "Female",
+        "Department": "Human Resources",
+        "Date of Joining": "08/22/2018",
+        "Salary": "$32940.53",
+        "City": "Valencia"
+    },
+    {
+        "Employee ID": "9141760",
+        "Employee Name": "Maurine McGreal",
+        "Gender": "Female",
+        "Department": "Accounting",
+        "Date of Joining": "02/19/2016",
+        "Salary": "$12769.67",
+        "City": "Thị Xã Lai Châu"
+    },
+    {
+        "Employee ID": "0440371",
+        "Employee Name": "Abby Marcum",
+        "Gender": "Male",
+        "Department": "Legal",
+        "Date of Joining": "03/12/2019",
+        "Salary": "$18565.98",
+        "City": "Lachute"
+    },
+    {
+        "Employee ID": "4319060",
+        "Employee Name": "Colet Dreghorn",
+        "Gender": "Male",
+        "Department": "Marketing",
+        "Date of Joining": "11/02/2018",
+        "Salary": "$36075.62",
+        "City": "Cincinnati"
+    },
+    {
+        "Employee ID": "8524811",
+        "Employee Name": "Morey Ilyin",
+        "Gender": "Male",
+        "Department": "Sales",
+        "Date of Joining": "07/13/2018",
+        "Salary": "$19845.66",
+        "City": "Liudu"
+    },
+    {
+        "Employee ID": "3536836",
+        "Employee Name": "Justus Eilert",
+        "Gender": "Male",
+        "Department": "Services",
+        "Date of Joining": "08/30/2016",
+        "Salary": "$25054.73",
+        "City": "Kitamilo"
+    },
+    {
+        "Employee ID": "6035100",
+        "Employee Name": "Lorelle Whyberd",
+        "Gender": "Female",
+        "Department": "Marketing",
+        "Date of Joining": "09/16/2010",
+        "Salary": "$21552.49",
+        "City": "Qandala"
+    },
+    {
+        "Employee ID": "9595117",
+        "Employee Name": "Hans Sponer",
+        "Gender": "Male",
+        "Department": "Legal",
+        "Date of Joining": "11/25/2019",
+        "Salary": "$12751.78",
+        "City": "Parakou"
+    },
+    {
+        "Employee ID": "9251706",
+        "Employee Name": "Esme Beaglehole",
+        "Gender": "Female",
+        "Department": "Engineering",
+        "Date of Joining": "11/24/2017",
+        "Salary": "$34244.64",
+        "City": "Saint-Ambroise"
+    },
+    {
+        "Employee ID": "5041677",
+        "Employee Name": "Fernandina Grindrod",
+        "Gender": "Female",
+        "Department": "Marketing",
+        "Date of Joining": "01/21/2019",
+        "Salary": "$25722.37",
+        "City": "Huashi"
+    },
+    {
+        "Employee ID": "9596007",
+        "Employee Name": "Glen Lambrechts",
+        "Gender": "Male",
+        "Department": "Human Resources",
+        "Date of Joining": "09/25/2019",
+        "Salary": "$47190.81",
+        "City": "Krajan Nglinggis"
+    },
+    {
+        "Employee ID": "3506310",
+        "Employee Name": "Judy Crick",
+        "Gender": "Female",
+        "Department": "Human Resources",
+        "Date of Joining": "10/22/2013",
+        "Salary": "$14168.85",
+        "City": "Jianchang"
+    },
+    {
+        "Employee ID": "0283605",
+        "Employee Name": "Megen McSherry",
+        "Gender": "Female",
+        "Department": "Engineering",
+        "Date of Joining": "10/19/2016",
+        "Salary": "$49673.94",
+        "City": "Talisayan"
+    },
+    {
+        "Employee ID": "5918336",
+        "Employee Name": "Aloysius Blunden",
+        "Gender": "Male",
+        "Department": "Services",
+        "Date of Joining": "06/16/2010",
+        "Salary": "$23992.84",
+        "City": "Italó"
+    },
+    {
+        "Employee ID": "0858107",
+        "Employee Name": "Colene Game",
+        "Gender": "Female",
+        "Department": "Training",
+        "Date of Joining": "10/18/2015",
+        "Salary": "$21768.47",
+        "City": "Podbrdo"
+    },
+    {
+        "Employee ID": "7239057",
+        "Employee Name": "Bruce Pook",
+        "Gender": "Male",
+        "Department": "Services",
+        "Date of Joining": "08/15/2019",
+        "Salary": "$46169.78",
+        "City": "Drawsko"
+    },
+    {
+        "Employee ID": "2603324",
+        "Employee Name": "Reggie Gethins",
+        "Gender": "Female",
+        "Department": "Training",
+        "Date of Joining": "05/03/2014",
+        "Salary": "$48793.33",
+        "City": "Baiqiao"
+    },
+    {
+        "Employee ID": "8641019",
+        "Employee Name": "Happy Terrell",
+        "Gender": "Female",
+        "Department": "Product Management",
+        "Date of Joining": "01/07/2010",
+        "Salary": "$11971.89",
+        "City": "Bandar-e Lengeh"
+    },
+    {
+        "Employee ID": "2229621",
+        "Employee Name": "Dermot Swithenby",
+        "Gender": "Male",
+        "Department": "Business Development",
+        "Date of Joining": "05/31/2017",
+        "Salary": "$14627.52",
+        "City": "Da’an"
+    },
+    {
+        "Employee ID": "6507226",
+        "Employee Name": "Dusty Naulls",
+        "Gender": "Female",
+        "Department": "Engineering",
+        "Date of Joining": "05/17/2012",
+        "Salary": "$49569.60",
+        "City": "Kelowna"
+    },
+    {
+        "Employee ID": "9588238",
+        "Employee Name": "Boycey Gemlbett",
+        "Gender": "Male",
+        "Department": "Accounting",
+        "Date of Joining": "08/19/2014",
+        "Salary": "$11400.60",
+        "City": "Quezon"
+    },
+    {
+        "Employee ID": "4783061",
+        "Employee Name": "Gregoire Craik",
+        "Gender": "Male",
+        "Department": "Support",
+        "Date of Joining": "01/07/2015",
+        "Salary": "$28536.96",
+        "City": "Sacramento"
+    },
+    {
+        "Employee ID": "1086662",
+        "Employee Name": "Paquito Keetch",
+        "Gender": "Male",
+        "Department": "Legal",
+        "Date of Joining": "07/20/2017",
+        "Salary": "$10948.94",
+        "City": "Xieshui"
+    },
+    {
+        "Employee ID": "7111790",
+        "Employee Name": "Siouxie Lippini",
+        "Gender": "Female",
+        "Department": "Engineering",
+        "Date of Joining": "02/08/2012",
+        "Salary": "$42688.51",
+        "City": "Bigoudine"
+    },
+    {
+        "Employee ID": "9189124",
+        "Employee Name": "Valerye Russell",
+        "Gender": "Female",
+        "Department": "Services",
+        "Date of Joining": "07/17/2012",
+        "Salary": "$32651.96",
+        "City": "Pichilemu"
+    },
+    {
+        "Employee ID": "6015219",
+        "Employee Name": "Patience Ludman",
+        "Gender": "Female",
+        "Department": "Training",
+        "Date of Joining": "09/25/2018",
+        "Salary": "$17589.61",
+        "City": "Binjiang"
+    },
+    {
+        "Employee ID": "5210163",
+        "Employee Name": "Helene Borsay",
+        "Gender": "Female",
+        "Department": "Services",
+        "Date of Joining": "01/19/2018",
+        "Salary": "$34017.42",
+        "City": "Chengkan"
+    },
+    {
+        "Employee ID": "6656266",
+        "Employee Name": "Garrek Fatharly",
+        "Gender": "Male",
+        "Department": "Business Development",
+        "Date of Joining": "08/22/2018",
+        "Salary": "$49775.81",
+        "City": "San Antonio Oeste"
+    },
+    {
+        "Employee ID": "8740096",
+        "Employee Name": "Fulton Culverhouse",
+        "Gender": "Male",
+        "Department": "Product Management",
+        "Date of Joining": "01/11/2014",
+        "Salary": "$36633.61",
+        "City": "Borlänge"
+    },
+    {
+        "Employee ID": "7875847",
+        "Employee Name": "Lavena Yandle",
+        "Gender": "Female",
+        "Department": "Accounting",
+        "Date of Joining": "08/31/2016",
+        "Salary": "$25835.58",
+        "City": "Borino"
+    },
+    {
+        "Employee ID": "8436792",
+        "Employee Name": "Sophia Cowwell",
+        "Gender": "Female",
+        "Department": "Product Management",
+        "Date of Joining": "08/06/2011",
+        "Salary": "$46836.53",
+        "City": "Fécamp"
+    },
+    {
+        "Employee ID": "2789522",
+        "Employee Name": "Dode Bowmen",
+        "Gender": "Female",
+        "Department": "Sales",
+        "Date of Joining": "02/21/2010",
+        "Salary": "$27575.10",
+        "City": "Karangwaru"
+    },
+    {
+        "Employee ID": "2816501",
+        "Employee Name": "Peggi Grishelyov",
+        "Gender": "Female",
+        "Department": "Accounting",
+        "Date of Joining": "12/08/2012",
+        "Salary": "$41712.59",
+        "City": "San Antonio"
+    },
+    {
+        "Employee ID": "5648843",
+        "Employee Name": "Ailsun Porker",
+        "Gender": "Female",
+        "Department": "Services",
+        "Date of Joining": "11/23/2016",
+        "Salary": "$46533.06",
+        "City": "Mayisyan"
+    },
+    {
+        "Employee ID": "3645602",
+        "Employee Name": "Licha McKee",
+        "Gender": "Female",
+        "Department": "Research and Development",
+        "Date of Joining": "08/29/2018",
+        "Salary": "$31008.69",
+        "City": "Wenqiao"
+    },
+    {
+        "Employee ID": "7413262",
+        "Employee Name": "Ives Tunn",
+        "Gender": "Male",
+        "Department": "Product Management",
+        "Date of Joining": "08/31/2018",
+        "Salary": "$27791.30",
+        "City": "Tabia"
+    },
+    {
+        "Employee ID": "9348082",
+        "Employee Name": "Cathy Bugdale",
+        "Gender": "Female",
+        "Department": "Training",
+        "Date of Joining": "04/23/2016",
+        "Salary": "$31759.16",
+        "City": "Azul"
+    },
+    {
+        "Employee ID": "7109150",
+        "Employee Name": "Waverley Gingedale",
+        "Gender": "Male",
+        "Department": "Accounting",
+        "Date of Joining": "09/04/2011",
+        "Salary": "$27893.84",
+        "City": "Frýdek-Místek"
+    },
+    {
+        "Employee ID": "1119404",
+        "Employee Name": "Cooper Capes",
+        "Gender": "Male",
+        "Department": "Sales",
+        "Date of Joining": "01/02/2016",
+        "Salary": "$26407.41",
+        "City": "Ash Sharyah"
+    },
+    {
+        "Employee ID": "9560117",
+        "Employee Name": "Shayna Potebury",
+        "Gender": "Female",
+        "Department": "Human Resources",
+        "Date of Joining": "10/23/2012",
+        "Salary": "$35332.31",
+        "City": "Baluk"
+    },
+    {
+        "Employee ID": "2598799",
+        "Employee Name": "Doro Gaitone",
+        "Gender": "Female",
+        "Department": "Sales",
+        "Date of Joining": "02/04/2012",
+        "Salary": "$22904.55",
+        "City": "Wulingyuan"
+    },
+    {
+        "Employee ID": "6767113",
+        "Employee Name": "Chantal Ventam",
+        "Gender": "Female",
+        "Department": "Training",
+        "Date of Joining": "07/02/2014",
+        "Salary": "$25515.12",
+        "City": "Xinshui"
+    },
+    {
+        "Employee ID": "3735396",
+        "Employee Name": "Tobie Brodeur",
+        "Gender": "Male",
+        "Department": "Training",
+        "Date of Joining": "08/11/2015",
+        "Salary": "$24508.84",
+        "City": "Xin’an"
+    },
+    {
+        "Employee ID": "2942841",
+        "Employee Name": "Feodor MacDermid",
+        "Gender": "Male",
+        "Department": "Human Resources",
+        "Date of Joining": "06/02/2019",
+        "Salary": "$14844.80",
+        "City": "Rantauambacang"
+    },
+    {
+        "Employee ID": "1511001",
+        "Employee Name": "Allyce Decker",
+        "Gender": "Female",
+        "Department": "Support",
+        "Date of Joining": "04/14/2017",
+        "Salary": "$39356.65",
+        "City": "Saint Paul"
+    },
+    {
+        "Employee ID": "9062663",
+        "Employee Name": "Ricard Connock",
+        "Gender": "Male",
+        "Department": "Product Management",
+        "Date of Joining": "11/25/2010",
+        "Salary": "$27421.61",
+        "City": "Lokorae"
+    },
+    {
+        "Employee ID": "6210073",
+        "Employee Name": "Minerva Greenham",
+        "Gender": "Female",
+        "Department": "Support",
+        "Date of Joining": "05/23/2014",
+        "Salary": "$48300.27",
+        "City": "Pakemitan"
+    },
+    {
+        "Employee ID": "1830385",
+        "Employee Name": "Cyb Gallant",
+        "Gender": "Female",
+        "Department": "Accounting",
+        "Date of Joining": "11/25/2010",
+        "Salary": "$36518.72",
+        "City": "Berëzovyy"
+    },
+    {
+        "Employee ID": "7466629",
+        "Employee Name": "Harman Free",
+        "Gender": "Male",
+        "Department": "Human Resources",
+        "Date of Joining": "07/15/2012",
+        "Salary": "$35939.87",
+        "City": "Watodei"
+    },
+    {
+        "Employee ID": "2963633",
+        "Employee Name": "Kylie Phettis",
+        "Gender": "Female",
+        "Department": "Marketing",
+        "Date of Joining": "03/18/2011",
+        "Salary": "$26038.56",
+        "City": "Huangzhai"
+    }
+  ];
