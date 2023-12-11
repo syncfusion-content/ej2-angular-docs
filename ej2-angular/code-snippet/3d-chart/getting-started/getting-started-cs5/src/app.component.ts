77d32f50e02c470c27b61bfd8d4552492f8bc502
Build Automaion <buildautomation@syncfusion.com>
2023-12-12 03:19:06 +0530

    Integrated latest changes at 12-12-2023 1:30:18 AM

---
+++ ej2-angular/code-snippet/3d-chart/getting-started/getting-started-cs5/src/app.component.ts
@@ -0,0 +1,43 @@
+
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-container',
+    template: `<ejs-chart3d style='display:block;' align='center' [primaryXAxis]='primaryXAxis' [primaryYAxis]='primaryYAxis'
+    rotation=7 tilt=10 depth=100 [enableRotation]='enableRotation'>
+    <e-chart3d-series-collection>
+        <e-chart3d-series [dataSource]='dataSource' [dataLabel]="datalabel" type='Column' xName='x' yName='y' name='Gold'>
+        </e-chart3d-series>
+    </e-chart3d-series-collection>
+</ejs-chart3d>`
+})
+export class AppComponent implements OnInit {
+    public dataSource: Object[] = [
+        { x: 'Tesla', y: 137429 },{ x: 'Aion', y: 80308 },
+        { x: 'Wuling', y: 76418 }, { x: 'Changan', y: 52849 },
+        { x: 'Geely', y: 47234 }, { x: 'Nio', y: 31041 },
+        { x: 'Neta', y: 22449 }, { x: 'BMW', y: 18733 }];
+    
+    //Initializing Primary X Axis
+    public primaryXAxis: Object = {
+        valueType: 'Category',
+        labelRotation:  -45,
+        labelPlacement: 'BetweenTicks'
+    };
+    public datalabel: Object = {
+        visible: true
+    };
+    //Initializing Primary Y Axis
+    public primaryYAxis: Object = {
+        maximum: 150000,
+        interval: 50000
+    };
+    //Initializing 3D-Chart Title
+    public enableRotation: boolean = true;
+
+}
+
+
+
---
+++ ej2-angular/code-snippet/common/state-persistence-cs1/src/datasource.ts
@@ -0,0 +1,47 @@
+export let data: Object[] = [
+    {
+        OrderID: 10248, CustomerID: 'VINET', OrderDate: new Date(8364186e5), Freight: 32.38,
+    },
+    {
+        OrderID: 10249, CustomerID: 'TOMSP', OrderDate: new Date(836505e6), Freight: 11.61
+    },
+    {
+        OrderID: 10250, CustomerID: 'HANAR', OrderDate: new Date(8367642e5), Freight: 65.83
+    },
+    {
+        OrderID: 10251, CustomerID: 'VICTE', OrderDate: new Date(8367642e5), Freight: 41.34
+    },
+    {
+        OrderID: 10252, CustomerID: 'SUPRD', OrderDate: new Date(8368506e5), Freight: 51.3
+    },
+    {
+        OrderID: 10253, CustomerID: 'HANAR', OrderDate: new Date(836937e6), Freight: 58.17
+    },
+    {
+        OrderID: 10254, CustomerID: 'CHOPS', OrderDate: new Date(8370234e5), Freight: 22.98
+    },
+    {
+        OrderID: 10255, CustomerID: 'RICSU', OrderDate: new Date(8371098e5), Freight: 148.33
+    },
+    {
+        OrderID: 10256, CustomerID: 'WELLI', OrderDate: new Date(837369e6), Freight: 13.97
+    },
+    {
+        OrderID: 10257, CustomerID: 'HILAA', OrderDate: new Date(8374554e5), Freight: 81.91
+    },
+    {
+        OrderID: 10258, CustomerID: 'ERNSH', OrderDate: new Date(8375418e5), Freight: 140.51
+    },
+    {
+        OrderID: 10259, CustomerID: 'CENTC', OrderDate: new Date(8376282e5), Freight: 3.25
+    },
+    {
+        OrderID: 10260, CustomerID: 'OTTIK', OrderDate: new Date(8377146e5), Freight: 55.09
+    },
+    {
+        OrderID: 10261, CustomerID: 'QUEDE', OrderDate: new Date(8377146e5), Freight: 3.05
+    },
+    {
+        OrderID: 10262, CustomerID: 'RATTC', OrderDate: new Date(8379738e5), Freight: 48.29
+    }
+];
