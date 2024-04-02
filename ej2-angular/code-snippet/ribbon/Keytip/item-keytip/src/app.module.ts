0918a562d70ecd1d5d4f95c54b097fa6231b7281
Build Automaion <buildautomation@syncfusion.com>
2024-04-03 03:28:24 +0530

    Integrated latest changes at 04-03-2024 1:30:13 AM

--- ej2-angular/code-snippet/ribbon/Keytip/item-keytip/src/app.module.ts
+++ ej2-angular/code-snippet/ribbon/Keytip/item-keytip/src/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 // Import Syncfusion Ribbon module from ribbon package.
-import { RibbonModule, RibbonGroupButtonService, RibbonGalleryService, RibbonKeyTipService } from '@syncfusion/ej2-angular-ribbon';
+import { RibbonModule, RibbonGroupButtonService, RibbonGalleryService, RibbonKeyTipService, RibbonColorPickerService } from '@syncfusion/ej2-angular-ribbon';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     imports: [BrowserModule, RibbonModule], // Registering EJ2 Ribbon Module.
-    providers: [ RibbonGroupButtonService, RibbonGalleryService, RibbonKeyTipService ],
+    providers: [ RibbonGroupButtonService, RibbonGalleryService, RibbonKeyTipService, RibbonColorPickerService ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
