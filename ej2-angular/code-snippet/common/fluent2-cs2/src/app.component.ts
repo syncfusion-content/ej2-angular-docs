2a789df9a9a1fec33cf310abc9beacc16d2cf9d7
Build Automaion <buildautomation@syncfusion.com>
2024-08-01 00:23:34 +0530

    Integrated latest changes at 07-31-2024 10:30:09 PM

--- ej2-angular/code-snippet/common/fluent2-cs2/src/app.component.ts
+++ ej2-angular/code-snippet/common/fluent2-cs2/src/app.component.ts
@@ -16,7 +16,7 @@ imports: [
 
 standalone: true,
     selector: 'app-root',
-    styleUrls:['./style.css'],
+    styleUrls:['./styles.css'],
     template:`<div>   
      <!-- Primary Button - Used to represent a primary action. -->
      <button ejs-button cssClass="e-primary">Button</button>
