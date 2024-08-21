cd82c9dc0f3a5b1fcc419f2e041fde2db43baf88
Build Automaion <buildautomation@syncfusion.com>
2024-08-22 00:59:43 +0530

    Integrated latest changes at 08-21-2024 10:30:11 PM

--- ej2-angular/code-snippet/listbox/getting-started-cs13/src/app.component.ts
+++ ej2-angular/code-snippet/listbox/getting-started-cs13/src/app.component.ts
@@ -32,7 +32,7 @@ public data: { [key: string]: Object }[] = [
     { text: 'McLaren P1' },
     { text: 'Ferrari LaFerrari' }
 ];
-public selection = { mode: "Single", showCheckbox: true }
+public selection = { mode: "Single" }
 }
 
 
