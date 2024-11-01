e5fcbdd0d82c89a858335102fa2d4a70a9275d39
Build Automaion <buildautomation@syncfusion.com>
2024-11-02 03:43:51 +0530

    Integrated latest changes at 11-02-2024 1:30:12 AM

---
+++ ej2-angular/code-snippet/listbox/filter-listbox-cs1/src/app.component.ts
@@ -0,0 +1,42 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { ListBoxComponent , ListBoxAllModule} from '@syncfusion/ej2-angular-dropdowns'
+import { Query } from '@syncfusion/ej2-data';
+import { Component } from '@angular/core';
+import { getInstance } from '@syncfusion/ej2-base';
+import { ListBox } from '@syncfusion/ej2-dropdowns';
+
+@Component({
+imports: [
+        ListBoxAllModule
+    ],
+
+
+standalone: true,
+    selector: 'app-container',
+    template: `<div class="e-section-control">
+                Enter Tex: <input id='filterTextBox' type="text" (input)="onFilterChange($event)" placeholder="Enter text to filter">
+                <h4>Select your favorite car:</h4>
+                <ejs-listbox id="listbox"  [dataSource]="data"></ejs-listbox></div>`
+})
+
+export class AppComponent {
+    public data: { [key: string]: Object }[] = [
+        { "text": "Hennessey Venom", "id": "list-01" },
+        { "text": "Bugatti Chiron", "id": "list-02" },
+        { "text": "Bugatti Veyron Super Sport", "id": "list-03" },
+        { "text": "SSC Ultimate Aero", "id": "list-04" },
+        { "text": "Koenigsegg CCR", "id": "list-05" },
+        { "text": "McLaren F1", "id": "list-06" },
+        { "text": "Aston Martin One- 77", "id": "list-07" },
+        { "text": "Jaguar XJ220", "id": "list-08" },
+        { "text": "McLaren P1", "id": "list-09" },
+        { "text": "Ferrari LaFerrari", "id": "list-10" }
+    ];
+    onFilterChange(event: Event) {
+        const listBox = getInstance((document as any).getElementById("listbox"), ListBox) as ListBox;
+        listBox.filter(this.data, new Query().where('text', 'contains', (event.target as HTMLInputElement).value, true));
+    }
+}
+
+
+
