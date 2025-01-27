85c09525d75343c78a8804bd69531b1580748393
Build Automaion <buildautomation@syncfusion.com>
2025-01-28 01:30:25 +0530

    Integrated latest changes at 01-27-2025 10:30:13 PM

---
+++ ej2-angular/code-snippet/grid/hide-dates-cs1/src/app.component.ts
@@ -0,0 +1,78 @@
+import { NgModule,ViewChild } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
+import { GridModule, EditService, ToolbarService, SortService, PageService } from '@syncfusion/ej2-angular-grids'
+import { DatePickerAllModule } from '@syncfusion/ej2-angular-calendars'
+import { TimePickerModule } from '@syncfusion/ej2-angular-calendars'
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns'
+import { TextBoxModule } from '@syncfusion/ej2-angular-inputs'
+import { MultiSelectModule } from '@syncfusion/ej2-angular-dropdowns'
+import { AutoCompleteModule } from '@syncfusion/ej2-angular-dropdowns'
+import { Component, OnInit } from '@angular/core';
+import { data } from './datasource';
+import { EditSettingsModel, ToolbarItems,GridComponent } from '@syncfusion/ej2-angular-grids';
+
+@Component({
+imports: [
+        
+        GridModule,
+        DatePickerAllModule,
+        FormsModule,
+        TimePickerModule,
+        FormsModule,
+        TextBoxModule,
+        MultiSelectModule,
+        AutoCompleteModule,
+        DropDownListModule
+    ],
+
+providers: [EditService, ToolbarService, SortService, PageService],
+standalone: true,
+    selector: 'app-root',
+    template: `
+        <ejs-grid #grid [dataSource]="data" [editSettings]="editSettings" [toolbar]="toolbar" height="315px"   (actionBegin)='actionBegin($event)'>
+          <e-columns>
+            <e-column field="OrderID" headerText="Order ID" isPrimaryKey="true" textAlign="Right" width="120" [validationRules]='orderIDRules'></e-column>
+            <e-column field="CustomerID" headerText="Customer Name" width="120" [validationRules]='customerIDRules'></e-column>
+            <e-column field="OrderDate" headerText="Order Date" editType="datepickeredit" format="M/d/yy" textAlign="Right" [validationRules]='dateRules' width="130" type="date"></e-column>
+            <e-column field="Freight" headerText="Freight" format="C2" textAlign="Right" width="90" [validationRules]='freightRules'></e-column>
+          </e-columns>
+        </ejs-grid>`
+})
+export class AppComponent implements OnInit {
+  @ViewChild('grid') public grid?: GridComponent;
+    public data?: object[];
+    public editSettings?: EditSettingsModel;
+    public toolbar?: ToolbarItems[];
+    public orderData?: object|any;
+    public orderIDRules?: object;
+    public customerIDRules?: object;
+    public freightRules?: object;
+    public dateRules?: object;
+    public verifiedRules?: object;
+
+    ngOnInit(): void {
+        this.data = data;
+        this.orderIDRules = { required: true };
+        this.customerIDRules = { required: true  };
+        this.dateRules= { required: true  };
+        this.verifiedRules= { required: true  };
+        this.freightRules = { required: true, min: 1, max: 1000 };
+        this.editSettings = {
+            allowEditing: true,
+            allowAdding: true,
+            allowDeleting: true,
+        };
+        this.toolbar = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
+    }
+    actionBegin(args: any): void {
+      if (args.requestType === 'beginEdit') {
+        let minDate = (args as any).rowData['OrderDate'];
+        (this.grid as GridComponent).columns.forEach((column) => {
+          if ((column as any).field === 'OrderDate') {
+            (column as any).edit.params = { min: minDate };
+          }
+        });
+      }
+    }
+}
