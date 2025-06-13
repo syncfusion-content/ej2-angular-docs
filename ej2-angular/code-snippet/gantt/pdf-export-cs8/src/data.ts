c8457ad994706e8c2d5d8cbd75c41c7222340903
Build Automaion <buildautomation@syncfusion.com>
2025-06-14 04:37:29 +0530

    Integrated latest changes at 06-13-2025 10:34:18 PM

--- ej2-angular/code-snippet/gantt/pdf-export-cs8/src/data.ts
+++ ej2-angular/code-snippet/gantt/pdf-export-cs8/src/data.ts
@@ -1,82 +1,10 @@
-/**
- * Gantt DataSource
- */
-
-export let editingResources: Object[] = [
-    { resourceId: 1, resourceName: 'Martin Tamer' },
-    { resourceId: 2, resourceName: 'Rose Fuller' },
-    { resourceId: 3, resourceName: 'Margaret Buchanan' },
-    { resourceId: 4, resourceName: 'Fuller King' },
-    { resourceId: 5, resourceName: 'Davolio Fuller' },
-    { resourceId: 6, resourceName: 'Van Jack' },
-    { resourceId: 7, resourceName: 'Fuller Buchanan' },
-    { resourceId: 8, resourceName: 'Jack Davolio' },
-    { resourceId: 9, resourceName: 'Tamer Vinet' },
-    { resourceId: 10, resourceName: 'Vinet Fuller' },
-    { resourceId: 11, resourceName: 'Bergs Anton' },
-    { resourceId: 12, resourceName: 'Construction Supervisor' }
-];
-
 export let editingData: Object[] = [
-    {
-        TaskID: 1,
-        TaskName: 'Project Initiation',
-        StartDate: new Date('04/02/2019'),
-        EndDate: new Date('04/21/2019'),
-    },
-    { TaskID: 2, TaskName: 'Identify Site location', StartDate: new Date('04/02/2019'), Duration: 4, ParentID:1, Progress: 90 },
-    { TaskID: 3, TaskName: 'Perform Soil test', StartDate: new Date('04/02/2019'), Duration: 4, Predecessor:2, ParentID:1, Progress: 40  },
-    { TaskID: 4, TaskName: 'Soil test approval', StartDate: new Date('04/02/2019'), Duration: 4, ParentID:1, Progress: 10 },      
-    {
-        TaskID: 5,
-        TaskName: 'Project Estimation',
-        StartDate: new Date('04/02/2019'),
-        EndDate: new Date('04/21/2019'),
-    },
-    { TaskID: 6, TaskName: 'Develop floor plan for estimation', StartDate: new Date('04/04/2019'), Duration: 3, ParentID:5, Progress: 85 },
-    { TaskID: 7, TaskName: 'List materials', StartDate: new Date('04/04/2019'), Duration: 3, ParentID:5, Progress: 15 },
-    { TaskID: 8, TaskName: 'Estimation approval', StartDate: new Date('04/04/2019'), Duration: 3, ParentID:5, Progress: 70 }
-];
-
-
-export let projectResources: Object[] = [
-    { resourceId: 1, resourceName: 'Project Manager' },
-    { resourceId: 2, resourceName: 'Software Analyst' },
-    { resourceId: 3, resourceName: 'Developer' },
-    { resourceId: 4, resourceName: 'Testing Engineer' }
-];
-
-export let projectData: Object[] = [
-    {
-        taskID: 1,
-        taskName: 'Project Schedule',
-        startDate: new Date('02/08/2019'),
-        endDate: new Date('03/15/2019'),
-        subtasks: [
-            {
-                taskID: 2,
-                taskName: 'Planning',
-                startDate: new Date('02/08/2019'),
-                endDate: new Date('02/12/2019'),
-                subtasks: [
-                    {
-                        taskID: 3, taskName: 'Plan timeline', startDate: new Date('02/08/2019'),
-                        endDate: new Date('02/12/2019'), duration: 5, progress: '100', resourceId: [1]
-                    },
-                    {
-                        taskID: 4, taskName: 'Plan budget', startDate: new Date('02/08/2019'),
-                        endDate: new Date('02/12/2019'), duration: 5, progress: '100', resourceId: [1]
-                    },
-                    {
-                        taskID: 5, taskName: 'Allocate resources', startDate: new Date('02/08/2019'),
-                        endDate: new Date('02/12/2019'), duration: 5, progress: '100', resourceId: [1]
-                    },
-                    {
-                        taskID: 6, taskName: 'Planning complete', startDate: new Date('02/10/2019'),
-                        endDate: new Date('02/10/2019'), duration: 0, predecessor: '3FS,4FS,5FS'
-                    }
-                ]
-            },
-        ]
-    }
+    { TaskId: 1, TaskName: 'Project Initiation', StartDate: new Date('04/02/2019'), EndDate: new Date('04/21/2019'), },
+    { TaskId: 2, TaskName: 'Identify Site location', StartDate: new Date('04/02/2019'), Duration: 4, ParentId:1, Progress: 90 },
+    { TaskId: 3, TaskName: 'Perform Soil test', StartDate: new Date('04/02/2019'), Duration: 4, Predecessor:2, ParentId:1, Progress: 40  },
+    { TaskId: 4, TaskName: 'Soil test approval', StartDate: new Date('04/02/2019'), Duration: 4, ParentId:1, Progress: 10 },      
+    { TaskId: 5, TaskName: 'Project Estimation', StartDate: new Date('04/02/2019'), EndDate: new Date('04/21/2019'), },
+    { TaskId: 6, TaskName: 'Develop floor plan for estimation', StartDate: new Date('04/04/2019'), Duration: 3, ParentId:5, Progress: 85 },
+    { TaskId: 7, TaskName: 'List materials', StartDate: new Date('04/04/2019'), Duration: 3, ParentId:5, Progress: 15 },
+    { TaskId: 8, TaskName: 'Estimation approval', StartDate: new Date('04/04/2019'), Duration: 3, ParentId:5, Progress: 70 }
 ];
