b92617b8dafef7405ae48da3b0862cec293cea30
Build Automaion <buildautomation@syncfusion.com>
2025-12-12 18:58:23 +0530

    Integrated latest changes at 12-12-2025 5:24:30 PM

---
+++ ej2-angular/code-snippet/ai-assistview/toolbar-items/footer-position/src/app.component.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AIAssistViewModule } from '@syncfusion/ej2-angular-interactive-chat';
+
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { AIAssistViewComponent, FooterToolbarSettingsModel, ToolbarItemClickedEventArgs } from '@syncfusion/ej2-angular-interactive-chat';
+import { PromptRequestEventArgs } from '@syncfusion/ej2-interactive-chat';
+
+@Component({
+    imports: [ FormsModule, ReactiveFormsModule, AIAssistViewModule ],
+    standalone: true,
+    selector: 'app-root',
+    // specifies the template string for the AI AssistView component
+    template: `<button id="toolbarBtn" class="e-btn" (click)="UpdateToolbarPosition()">UpdateToolbarPosition</button>
+    <div ejs-aiassistview #aiAssistViewComponent (promptRequest)="onPromptRequest($event)" [footerToolbarSettings]="footerToolbarSettings" [prompts]="promptsData"></div>`
+})
+
+export class AppComponent {
+    @ViewChild('aiAssistViewComponent')
+    public aiAssistViewComponent!: AIAssistViewComponent;
+
+    public promptsData = [
+        {
+          prompt: "What is AI?",
+          response: `<div>AI stands for Artificial Intelligence, enabling machines to mimic human intelligence for tasks such as learning, problem-solving, and decision-making.</div>`
+        }
+    ];
+
+    public footerToolbarSettings: FooterToolbarSettingsModel = {
+        toolbarPosition: 'Bottom'
+    };
+    
+    public onPromptRequest = (args: PromptRequestEventArgs) => {
+        setTimeout(() => {
+          let foundPrompt = this.promptsData.find((promptObj: any) => promptObj.prompt === args.prompt);
+          let defaultResponse = 'For real-time prompt processing, connect the AIAssistView component to your preferred AI service, such as OpenAI or Azure Cognitive Services. Ensure you obtain the necessary API credentials to authenticate and enable seamless integration.';
+          this.aiAssistViewComponent.addPromptResponse(foundPrompt ? foundPrompt.response : defaultResponse);
+        }, 1000);
+    };
+
+    public UpdateToolbarPosition = () => {
+      if (this.aiAssistViewComponent.footerToolbarSettings.toolbarPosition === 'Inline') {
+        this.aiAssistViewComponent.footerToolbarSettings.toolbarPosition = 'Bottom';
+      }
+      else {
+        this.aiAssistViewComponent.footerToolbarSettings.toolbarPosition = 'Inline';
+      }
+    }
+}
