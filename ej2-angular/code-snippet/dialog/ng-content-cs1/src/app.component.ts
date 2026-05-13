d12a0739b65f10774c89772633732fb13ca0e0e2
Build Automaion <buildautomation@syncfusion.com>
2026-05-14 02:03:12 +0530

    Integrated latest changes at 05-14-2026 1:30:03 AM

--- ej2-angular/code-snippet/dialog/ng-content-cs1/src/app.component.ts
+++ ej2-angular/code-snippet/dialog/ng-content-cs1/src/app.component.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { DialogModule } from '@syncfusion/ej2-angular-popups'
-
-
-
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DialogModule, DialogComponent } from '@syncfusion/ej2-angular-popups';
 import { Component, ViewChild } from '@angular/core';
-import { DialogComponent } from '@syncfusion/ej2-angular-popups';
+
 @Component({
-imports: [
-        
-		DialogModule
-    ],
+  standalone: true,
+  selector: 'app-ng-content',
+  template: `<div class="dialog-content-wrapper">
+      <ng-content></ng-content>
+</div>`,
+})
+export class NgContentComponent {}
 
-
-standalone: true,
+@Component({
+  imports: [DialogModule, NgContentComponent],
+  standalone: true,
   selector: 'app-root',
-  template: `<div class="control-section">
+  template: `<div class="control-section" style="margin: 100px auto; width: 50%" >
   <button class="e-btn" id='dlgbtn' #ButtonInstance (click)="BtnClick()">Open</button>
      <!-- Render Button to open the Dialog -->
      <ejs-dialog #Dialog [buttons]='dlgButtons' [header]='header' [animationSettings]='animationSettings' [showCloseIcon]='showCloseIcon' [width]='width' [height]='height' (open)="dialogOpen()"
@@ -25,47 +25,38 @@ standalone: true,
          <span> 10% of battery remaining </span>
          </app-ng-content>
      </ejs-dialog>
- </div>`
+ </div>`,
 })
-
 export class AppComponent {
   @ViewChild('Dialog')
-    public Dialog?: DialogComponent;
-    public showCloseIcon: Boolean = true;
-    public width: string = '250px';
-    public height: string = '150px';
-    public animationSettings: Object = { effect: 'None' };
-    public header: string = 'Low Battery';
-    BtnClick() {
-        this.Dialog!.show();
-    }
-    dialogClose() {
-        document.getElementById('dlgbtn')!.style.display = 'block';
-        }
-    // On Dialog open, 'Open' Button will be hidden
-    dialogOpen() {
-        document.getElementById('dlgbtn')!.style.display = 'none';
-    }
-    public dlgButtons: Object[] = [{
-        click: this.dlgBtnClick.bind(this),
-        buttonModel: { content: 'OK', isPrimary:'true' } },
-        { click: this.dlgBtnClick.bind(this), buttonModel: { content: 'Cancel' }
-      }];
+  public Dialog?: DialogComponent;
+  public showCloseIcon: Boolean = true;
+  public width: string = '250px';
+  public height: string = '150px';
+  public animationSettings: Object = { effect: 'None' };
+  public header: string = 'Low Battery';
+  BtnClick() {
+    this.Dialog!.show();
+  }
+  dialogClose() {
+    document.getElementById('dlgbtn')!.style.display = 'block';
+  }
+  // On Dialog open, 'Open' Button will be hidden
+  dialogOpen() {
+    document.getElementById('dlgbtn')!.style.display = 'none';
+  }
+  public dlgButtons: Object[] = [
+    {
+      click: this.dlgBtnClick.bind(this),
+      buttonModel: { content: 'OK', isPrimary: 'true' },
+    },
+    {
+      click: this.dlgBtnClick.bind(this),
+      buttonModel: { content: 'Cancel' },
+    },
+  ];
 
-    dlgBtnClick() {
+  dlgBtnClick() {
     this.Dialog!.hide();
-    }
+  }
 }
-
-@Component({
-  selector: 'app-ng-content',
-  template: `<div class="dialog-content-wrapper">
-      <ng-content></ng-content>
-</div>`
-})
-export class NgContentComponent {
-
-}
-
-
-
