f5f083c0bccac191a583bcabf1630ab4e213642a
Build Automaion <buildautomation@syncfusion.com>
2026-09-04 23:03:49 +0530

    Integrated latest changes at 09-04-2026 10:30:04 PM

--- ej2-angular/code-snippet/grid/filter-menu-cs1/src/main.ts
+++ ej2-angular/code-snippet/grid/filter-menu-cs1/src/main.ts
@@ -1,4 +1,5 @@
 import { AppComponent } from './app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
