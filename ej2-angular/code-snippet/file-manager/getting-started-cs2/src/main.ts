0afb974eafc3ff15decaa353b07cc9f537665ced
Build Automaion <buildautomation@syncfusion.com>
2026-09-18 02:01:18 +0530

    Integrated latest changes at 09-18-2026 1:30:05 AM

--- ej2-angular/code-snippet/file-manager/getting-started-cs2/src/main.ts
+++
@@ -1,4 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app.component';
-import 'zone.js';
-bootstrapApplication(App).catch((err) => console.error(err));
